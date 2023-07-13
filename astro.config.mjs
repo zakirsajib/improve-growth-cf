9bd9e18ac82d0ab822de386fa5192faf534544de
Zajur Sajib <zakirsajib@yahoo.co.uk>
2023-07-14 05:14:00 +0600

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://improve-growth-cloudflare.pages.dev',
+  output: 'server',
+  adapter: cloudflare(),
+  integrations: [sitemap()]
+});
