75c3bf9d3ec42c5fdf4ab93f49d50573ff492767
Zajur Sajib <zakirsajib@yahoo.co.uk>
2023-07-14 05:39:26 +0600

    cloudflare pages url is added

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://improve-growth-cloudflare.pages.dev',
+  site: 'https://improve-growth.pages.dev/',
   output: 'server',
   adapter: cloudflare(),
   integrations: [sitemap()]
